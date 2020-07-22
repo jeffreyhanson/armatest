65b6ca1b2b70226b8500d875edc760b4993cf848
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2020-07-22 18:11:47 +1200

    add working version

--- src/package.h
+++ src/package.h
@@ -1,15 +1,8 @@
-#pragma once
-
-// _Pragma("GCC diagnostic push")
-// _Pragma("GCC diagnostic ignored \"-Wodr\"")
-
 #ifndef PACKAGE
 #define PACKAGE
 
 #define ARMA_64BIT_WORD  //required to support arma vectors > 2GB
-// [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
-
 // [[Rcpp::plugins(cpp11)]]
 using namespace Rcpp;
 
